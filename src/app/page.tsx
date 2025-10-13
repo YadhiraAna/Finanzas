f1852a450f7f0627206dc4a131b4521fd52f844d
YadhiraAna <yayakim01412@gmail.com>
2025-10-13 08:33:55 -0600

    Primer commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <>
+      <header className="site-header">
+        <nav className="nav">
+          <div className="brand">
+            <img src="/pesito.png" alt="Pesito a Pesito" className="logo-img" />
+          </div>
+          <div className="menu">
+            <button className="btn">Iniciar Sesión</button>
+            <button className="btn">Registrarse</button>
+          </div> 
+        </nav>
+        </header>
+    </>
+  );
+ }
